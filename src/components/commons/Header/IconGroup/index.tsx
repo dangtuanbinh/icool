feef4ee7ecb801866dfaa2fdac323c4048c1e67f
Robin <tuanbinh.35ggmail.com>
2022-09-12 08:53:27 +0700

    fix UI error

--- src/components/commons/Header/IconGroup/index.tsx
+++ src/components/commons/Header/IconGroup/index.tsx
@@ -8,21 +8,21 @@ const IconGroup = () => {
   return (
     <div className="iconGroup">
       <a
-        href="https://discord.com/invite/HWk9kqy3Yg"
+        href="https://facebook.com"
         target="_blank"
         rel="noreferrer"
       >
         <FaFacebook size={20} color="#F96D06" />
       </a>
       <a
-        href="https://opensea.io/collection/suhosin-official"
+        href="https://instagram.com"
         target="_blank"
         rel="noreferrer"
       >
         <RiInstagramFill size={20} color="#F96D06" />
       </a>
       <a
-        href="https://twitter.com/SuhosinWorld"
+        href="https://twitter.com"
         target="_blank"
         rel="noreferrer"
       >
